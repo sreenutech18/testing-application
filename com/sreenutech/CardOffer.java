246d4f82af42e99f43dd88ac1cba410309b607dd
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2022-11-13 12:48:37 +0530

    Update CardOffer.java

--- com/sreenutech/CardOffer.java
+++ com/sreenutech/CardOffer.java
@@ -4,19 +4,19 @@ public class CardOffer{
 	
 	public void m1(){
 	
-		System.out.println("***m1()****");
+		System.out.println("***m1() method ****");
 		
 	}
 	public void m2(){
 		
-		System.out.println("***m2()****");
+		System.out.println("***m2() method ****");
 	}
 	
 	public static void main(String[] args){
 		
 		CardOffer offer = new CardOffer();
-		offer.m1();
-		offer.m2();
+		offer.m1(); // m1() method will call
+		offer.m2(); // m2() method will call
 	}	
 
 }	
