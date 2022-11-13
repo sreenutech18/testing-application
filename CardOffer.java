45940d8919e47fb086856b6cbb1a31b222560925
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2022-11-13 12:18:46 +0530

    Update CardOffer.java

--- CardOffer.java
+++ CardOffer.java
@@ -1,12 +1,20 @@
 public class CardOffers{
 	
 	public void m1(){
-		
+	
+		System.out.println("***m1()****");
 		
 	}
 	public void m2(){
 		
-		
+		System.out.println("***m2()****");
 	}
+	
+	public static void main(String[] args){
+		
+		CardOffers offer = new CardOffers();
+		offer.m1();
+		offer.m2();
+	}	
 
 }	
